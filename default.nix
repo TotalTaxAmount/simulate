f22590fb76160405adb4a1034f9fb003a068250b
TotalTaxAmount <shieldscoen@gmail.com>
2024-03-15 15:09:40 -0600

    some random stuff

--- default.nix
+++ default.nix
@@ -10,5 +10,17 @@ pkgs.stdenv.mkDerivation {
             pkg-config
             sparse
             glib
+
+            # Debuggung
+            binwalk
+            dtc
+            hexedit
+            (ubootTools.overrideAttrs (old: rec {
+              defaultVersion = "2016.09";
+              defaultSrc = pkgs.fetchurl {
+                url = "https://ftp.denx.de/pub/u-boot/u-boot-${defaultVersion}.tar.bz2";
+                hash = "sha256-mU57ng41DZ+R9YktXlv/ANNxSkli9SD3imSkYe+VC6k=";
+              };
+            }))
         ];
     }
